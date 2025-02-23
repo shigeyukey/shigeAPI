34ebb4f4eb488951f7e61b559c607431b92902eb
shigeyukey <shigge.3.21@gmail.com>
2025-02-24 01:41:45 +0900

    update

--- shigeAPI.py
+++ shigeAPI.py
@@ -3,9 +3,13 @@
 
 from .api_class import ShigeAPI
 
-# BreakTimer https://ankiweb.net/shared/info/174058935
+### BreakTimer ###
+# https://ankiweb.net/shared/info/174058935
+# Start the break timer now.
 start_break_timer = ShigeAPI("start_break_timer")
 
-# Leaserboard https://ankiweb.net/shared/info/175794613
+### Leaserboard ###
+# https://ankiweb.net/shared/info/175794613
+# Open the Leaderboard window.
 open_leaderboard = ShigeAPI("open_leaderboard")
 
