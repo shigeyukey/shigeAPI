128a29a0d4ff05d1fefd3608738619a3dff0a10b
shigeyukey <shigge.3.21@gmail.com>
2025-02-24 01:17:40 +0900

    update

---
+++ shigeAPI.py
@@ -0,0 +1,26 @@
+
+from aqt import mw
+
+class ShigeAPI():
+    def __init__(self, new_attr_name:str):
+        self.attr_name = "shigeAPI_" + new_attr_name
+
+    def run(self):
+        try:
+            if hasattr(mw, self.attr_name):
+                mw_addon_func = getattr(mw, self.attr_name)
+                if callable(mw_addon_func):
+                    mw_addon_func()
+        except Exception as e:
+            print(f"shigeAPI {self.attr_name} Error: {e}")
+
+    def add(self, func):
+        setattr(mw, self.attr_name, func)
+
+
+# BreakTimer https://ankiweb.net/shared/info/174058935
+start_break_timer = ShigeAPI("start_break_timer")
+
+# Leaserboard https://ankiweb.net/shared/info/175794613
+open_leaderboard = ShigeAPI("open_leaderboard")
+
